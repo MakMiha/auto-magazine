ffb9189d117536fdcebeb36788412d3c4d63b277
MakMiha <mackarowmixail@yandex.ru>
2021-10-11 13:54:22 +0300

    fix-1

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,32 @@ const prevImageButton = document.querySelector('.gallery__slider-prev');
 const review = document.querySelectorAll('.review__text');
 const dot = document.querySelectorAll('.review__dot');
 const reviewLink = document.querySelectorAll('.review__button');
+const menu = document.querySelector('.header__button-menu');
+const logoHeader = document.querySelector('.logo_place_header');
+const linksHeader = document.querySelector('.header__links');
+const formSubscribe = document.querySelector('form[name="form-subscribe"]');
+const buttonSubscribe = document.querySelector('.form-container__button');
 let imageIndex = 0;
 let dotIndex = 0;
+
+//Меню
+menu.addEventListener('click', (evt) =>{
+  evt.target.classList.toggle('header__button-menu_view_close');
+  logoHeader.classList.toggle('logo_display_active');
+  linksHeader.classList.toggle('header__links_display_active');
+});
+
+//Изменение текста кнопки в форме.
+formSubscribe.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  buttonSubscribe.innerText = 'Готово!';
+});
 
 //Предыдущая фотография
 nextImageButton.addEventListener('click', (evt) => {
   imageIndex--;
   imageIndex < 0 && (imageIndex = image.length-1);
-  showImage(imageIndex)
-  ;
+  showImage(imageIndex);
 });
 //Следующая фотография
 prevImageButton.addEventListener('click', (evt) => {
